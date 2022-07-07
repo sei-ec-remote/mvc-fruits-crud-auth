7d470c1d1ba3b35a9ff2d6657a73e8271d8f014b
bethparnell <beth.parnell@generalassemb.ly>
2022-07-07 07:43:57 -0400

    Update for cohort Ice Pops

---
+++ README.md
@@ -0,0 +1,2 @@
+# full-CRUD-fruits-mongoose
+Full CRUD build fruit app using express, mongoose, and liquid
---
+++ server.js
@@ -0,0 +1,39 @@
+/////////////////////////////////
+// import dependencies
+/////////////////////////////////
+// this allows us to load our env variables
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+
+////////////////////////////////////////////
+// Create our express application object
+////////////////////////////////////////////
+const app = require('liquid-express-views')(express())
+
+////////////////////////////////////////////
+// Middleware
+////////////////////////////////////////////
+// this is for request logging
+app.use(morgan('tiny'))
+app.use(methodOverride('_method'))
+// parses urlencoded request bodies
+app.use(express.urlencoded({ extended: false }))
+// to serve files from public statically
+app.use(express.static('public'))
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+app.get('/', (req, res) => {
+	res.send('your server is running, better go catch it')
+})
+
+////////////////////////////////////////////
+// Server Listener
+////////////////////////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+	console.log(`app is listening on port: ${PORT}`)
+})
